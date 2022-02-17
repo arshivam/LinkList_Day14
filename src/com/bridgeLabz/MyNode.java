bdfea11a996ddc02bfd478bff8db685d60af7688
arshivam <shivam.armarkar007@gmail.com>
2022-02-17 18:48:07 +0530

    Added code to add Elment at Top

--- src/com/bridgeLabz/MyNode.java
+++ src/com/bridgeLabz/MyNode.java
@@ -1,19 +1,30 @@
 package com.bridgeLabz;
 
-public class MyNode<K> {
-	private K key;
-	private MyNode next;
+public class MyNode<K> implements INode<K> {
+    private K key;
+    private INode next;
 
-	public MyNode(K key) {
-		this.key =key;
-		this.next=null;
-	}
+    public MyNode(K key) {
+        this.key =key;
+        this.next=null;
+    }
 
-	public MyNode getNext() {
-		return next;
-	}
+    @Override
+    public K getKey() {
+        return key;
+    }
 
-	public void setNext(MyNode next) {
-		this.next = next;
-	}
+    @Override
+    public void setKey(K key) {
+        this.key=key;
+    }
+
+    public INode getNext() {
+        return next;
+    }
+
+    @Override
+    public void setNext(INode next) {
+        this.next=(MyNode<K>) next;
+    }
 }
